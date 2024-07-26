652e44c973e50c5f327afbc7d6af4fb9715c2671
Seydou Diarrassouba <115084625+1Seydou-12-diarra@users.noreply.github.com>
2024-07-26 10:05:38 +0000

    initialisation_projet

---
+++ projet_sige/src/main/java/com/projet_sige/projet_sige/ProjetSigeApplication.java
@@ -0,0 +1,13 @@
+package com.projet_sige.projet_sige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetSigeApplication.class, args);
+	}
+
+}
---
+++ projet_sige/src/test/java/com/projet_sige/projet_sige/ProjetSigeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet_sige.projet_sige;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSigeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
